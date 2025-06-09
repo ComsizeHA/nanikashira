46ac901ac1d36c77dafb3055a99cd2fcf0f23b1f
arino <hisato.arino@comsize.com>
2025-06-10 00:37:30 +0900

    設定値保存デモ

--- test/background.js
+++ test/background.js
@@ -1,26 +1,3 @@
-chrome.action.onClicked.addListener((tab) => {
-  // utils.js を先に inject（補助関数群）
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["utils.js"]
-  }, () => {
-    // main 関数を実行
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: main
-    });
-  });
+chrome.action.onClicked.addListener(() => {
+  chrome.tabs.create({ url: chrome.runtime.getURL("view.html") });
 });
-
-// 実行のメイン関数（utils.js の関数を使う）
-function main() {
-  const h1 = document.querySelector('h1');
-  if (!h1) {
-    console.warn("h1 タグが見つかりません。");
-    return;
-  }
-
-  updateText(h1, "こんにちは、世界！");
-  highlight(h1);
-  logInfo("h1 を更新しました");
-}
